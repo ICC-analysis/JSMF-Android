4784a3f812e39f9d930779331cea2c5e2498f8eb
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-02-07 11:17:49 +0100

    added outputs of command in the js configuration file

--- conf.js
+++ conf.js
@@ -4,5 +4,7 @@ module.exports = {
     IC3Proto: './var/ic3/ic3data.proto',
     IC3ProtoGrammar: './var/ic3/grammar.pegjs',
     IC3EntryPoint: 'edu.psu.cse.siis.ic3.Application',
-    BinaryAppProtoBuf: './var/apps/krep.itmtd.ywtjexf_3.dat'  //'./var/apps/a2dp.Vol_107.dat'
+    BinaryAppProtoBuf: './var/apps/krep.itmtd.ywtjexf_3.dat',
+
+    bin_outputs: 'outputs/'
 };
