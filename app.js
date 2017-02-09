32414676d3496ef22c29300856d9c0566c78b43b
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-02-09 08:03:39 +0100

    Clean bad code.

--- app.js
+++ app.js
@@ -19,13 +19,15 @@ var IC3Proto = require('./conf.js').IC3Proto,
   IC3EntryPoint = require('./conf.js').IC3EntryPoint,
   BinaryAppProtoBuf = require('./conf.js').BinaryAppProtoBuf
 
+var apk_analyzer = require('./process');
+
 var protoBufModels =  require('builder');
-protoBufModels.build(IC3Proto, IC3ProtoGrammar,
-                      IC3EntryPoint, BinaryAppProtoBuf);
+// load the default model
+protoBufModels.build(IC3Proto, IC3ProtoGrammar, IC3EntryPoint,
+                                                        BinaryAppProtoBuf);
 
 var listening_port = process.env.PORT || 3000;
 
-var process_async = require('./process');
 
 app.use(session({
   secret: 'sessio0-Id',
@@ -51,14 +53,12 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 var upload = multer({ dest: 'uploads/' });
 
 
-
 http.listen(listening_port, function () {
   console.log('Listening on port ' + listening_port);
 });
 
 app.get('/', function (req, res) {
   var errorMessage = JSON.stringify('');
-
   res.render('index.html', {errorMessage: errorMessage });
 });
 
@@ -109,25 +109,22 @@ app.get('/models', function(req,res){
 });
 
 app.post('/upload', upload.single('file'), function(req, res, next) {
-
     var M = protoBufModels.model;
 
     if (req.file && req.file.originalname.split('.').pop() == "dat") {
-        // A binary protobuf is directly submitted
-        //relaunch the protobufModel construction
+        // A binary protobuf is directly submitted -
+        // simply relaunch the protobufModel construction.
         console.log('A binary protobuf file has been received: ' +
                                                         req.file.originalname)
-        protoBufModels.build(IC3Proto, IC3ProtoGrammar,
-            IC3EntryPoint, req.file.path);
+        protoBufModels.build(IC3Proto, IC3ProtoGrammar, IC3EntryPoint,
+                                                        req.file.path);
     }
 
     else if (req.file && req.file.originalname.split('.').pop() == "apk") {
-        // An APK is submitted
-        //log_web.logWebSocket(io, 'An APK file has been received: ' + req.file.originalname)
-        log_web_socket(io, 'An APK file has been received: ' + req.file.originalname)
-
-        process_async.start_process(req);
-
+        // An APK is submitted - launch the full process.
+        log_web_socket(io, 'An APK file has been received: ' +
+                                                        req.file.originalname)
+        apk_analyzer.start_process(req);
     }
 
     else {
